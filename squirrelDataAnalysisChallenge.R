b683173bef49f89db34a60529c47fdb63fc26698
Molly Johnson <molly211@gmail.com>
2020-08-16 20:53:13 +0000

    updated description

--- squirrelDataAnalysisChallenge.R
+++ squirrelDataAnalysisChallenge.R
@@ -1,4 +1,11 @@
+###########################################################################
 # Coding Club Stats From Scratch Course Challenge 1:
 # Red Squirrel Data Challenge
+# Description: Clean the dataset, determine if there is a temporal trend
+# in the number of observations for red and grey squirrels between
+# 2008 and 2017, determine if red and grey squirrels prefer different habitats,
+# and re-classify forest cover
 # Author: Molly Johnson, Oregon State University/Bradley University
 # Date: 8-14-2020
+###########################################################################
+
